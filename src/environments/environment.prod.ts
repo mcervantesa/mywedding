0fff4b1ebad289793ff8de388d65f80bec8f7ecf
mcervantesa <mariocervantes2205@gmail.com>
2024-01-13 14:53:08 -0600

    secind commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyAPbpXXNQHpwaMlZrpJK9fpIoLpQs9szZg",
+        authDomain: "mywedding-bba04.firebaseapp.com",
+        projectId: "mywedding-bba04",
+        storageBucket: "mywedding-bba04.appspot.com",
+        messagingSenderId: "606679211460",
+        appId: "1:606679211460:web:69eb972127d63d1e435891"
+    }
+};
